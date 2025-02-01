21eb001ffc19048a859ecce8611509075dc64c51
kranthibhagath <kranthi-bhagath.reddy.ext@veolia.com>
2025-02-01 21:04:27 +0530

    add files

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EvAllGo',
+  webDir: 'dist',
+  // platforms: ['android', 'ios'] // Add this line
+};
+
+export default config;
